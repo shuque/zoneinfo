b190610bc40f3a6963a64ebfa80d9b916fbcc04d
Shumon Huque <shuque@upenn.edu>
2012-01-02 22:12:29 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='zoneinfo',
+      version='0.1',
+      scripts=['zoneinfo'],
+      description='Query DNS zone information',
+      author='Shumon Huque',
+      author_email='shuque@upenn.edu',
+      url='http://www.huque.com/software/zoneinfo/',
+
+      long_description = \
+      """zoneinfo is a python program to obtain some information about a DNS zone.""",
+      )
